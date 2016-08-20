92edafb21346430400d35dc3dbefcc87ff846dc5
jvoigtlaender <jvoigtlaender@users.noreply.github.com>
2016-08-20 21:30:09 +0200

    Travis: simplify and new token

--- deploy.sh
+++ deploy.sh
@@ -14,4 +14,4 @@ git config user.name "Travis CI"
 git config user.email "jvoigtlaender@users.noreply.github.com"
 git add .
 git commit -m "Travis deploy to gh-pages"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages >/dev/null 2>&1
+git push --force --quiet "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master:gh-pages >/dev/null 2>&1
